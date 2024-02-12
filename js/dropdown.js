5699480c0b721836922579903e087c33a0917d19
MarijnW <wit.marijn@gmail.com>
2024-02-12 10:32:49 +0100

    styling fine-tuning

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,11 +1,27 @@
 $(".chart-bar").click(dropdown);
+$(".chart-header").click(dropdown2);
+
 
 function dropdown() {
-    if (this.childElementCount > 1 && this.clientHeight <= 30) {
+    // if dorpdown content contains an element and is not extended
+    if (this.childElementCount > 1 && !this.classList.contains("extended")) {
         this.classList.add("extended");
         $(this).find(".dropdown-icon")[0].innerHTML = "-"
-    } else if (this.clientHeight > 30) {
+        //if dropdown is extended
+    } else if (this.classList.contains("extended")) {
         this.classList.remove("extended");
+        $(this).find(".dropdown-icon")[0].innerHTML = "+"
+    }
+}
+
+function dropdown2() {
+    let targetElement = $(this).next()[0];
+
+    if (!targetElement.classList.contains("extended")) {
+        targetElement.classList.add("extended");
+        $(this).find(".dropdown-icon")[0].innerHTML = "-"
+    } else if (targetElement.classList.contains("extended")) {
+        targetElement.classList.remove("extended");
         $(this).find(".dropdown-icon")[0].innerHTML = "+"
     }
 }
